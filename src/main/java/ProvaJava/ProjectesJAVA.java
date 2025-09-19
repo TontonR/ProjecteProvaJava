26ac3db7124b376bd9c69c6654bcdd7fd2f93cc5
Ruben <rubenmb2007@gmail.com>
2025-09-19 12:34:14 +0200

    segona versio

--- src/main/java/ProvaJava/ProjectesJAVA.java
+++ src/main/java/ProvaJava/ProjectesJAVA.java
@@ -11,6 +11,6 @@ package ProvaJava;
 public class ProjectesJAVA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bon dia!");
     }
 }
